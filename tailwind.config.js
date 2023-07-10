4e036ed5a24eb6c51ca66ffc5c2e308a2fc0deb8
David Panzarella <david@deven.io>
2023-07-10 09:36:21 -0700

    Saving updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,30 @@
 module.exports = {
   content: [
     './layout/*.liquid',
-    './templates/*.liquid',
-    './templates/customers/*.liquid',
+    './templates/**/*.liquid',
     './sections/*.liquid',
     './snippets/*.liquid',
   ],
-  prefix: 'tw-',
-  theme: {
-    fontSize: {
-      base: '16px',
-    },
-    extend: {
-      //   fontFamily: {
-      //     heading: 'var(--font-heading-family)',
-      //   },
-    },
-    screens: {
-      sm: '320px',
-      md: '750px',
-      lg: '990px',
-      xlg: '1440px',
-      x2lg: '1920px',
-      pageMaxWidth: '1440px',
-    },
-    color: {
-      black: '#000000',
-    },
-  },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('tailwindcss-base-font-size'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
   ],
+  prefix: 'tw-',
+  theme: {
+    color: {
+      black: '#19181D',
+      bone: '#D6CABA',
+      orange: '#F2A100',
+    },
+    screens: {
+      sm: '320px',
+      md: '750px',
+      lg: '990px',
+      x2lg: '1920px',
+      xlg: '1440px',
+      pageMaxWidth: '1440px',
+    },
+  },
 };
